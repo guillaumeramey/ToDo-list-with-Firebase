0da5716c7e22b8c13c54d0e0b3513280f5db7f54
Guillaume Ramey <guillaume.ramey@gmail.com>
2022-01-22 16:26:04 +0100

    Initial Commit

---
+++ ToDo list with Firebase/ToDo_list_with_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_list_with_FirebaseApp.swift
+//  ToDo list with Firebase
+//
+//  Created by Guillaume Ramey on 22/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_list_with_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
